a0978d96664d169b2a74d2f04496aa78eb3db3f0
Mohamed Abdelmagid <MohamedAbdelmagid12@outlook.com>
2023-04-19 15:16:48 -0400

    Basics for class done.

--- program/data_persistence.py
+++ program/data_persistence.py
@@ -1,1 +1,12 @@
 # persist all data into csv file
+class CharacterPersistence:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load_characters(self):
+        # this will load the file
+        pass
+
+    def save_characters(self):
+        # this will save characters to the csv file
+        pass
