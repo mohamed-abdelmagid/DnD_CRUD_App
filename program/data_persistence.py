df85506dc4a5b6221d4387584140bdaae1cfd385
Mohamed Abdelmagid <MohamedAbdelmagid12@outlook.com>
2023-04-19 17:03:45 -0400

    Added load characters

--- program/data_persistence.py
+++ program/data_persistence.py
@@ -1,4 +1,5 @@
 import csv
+from character_manager import Character
 
 
 class CharacterPersistence:
@@ -6,8 +7,18 @@ class CharacterPersistence:
         self.file_path = file_path
 
     def load_characters(self):
-        # this will load the file
-        pass
+        characters = []
+        with open(self.file_path, mode='r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                id = row[0]
+                name = row[1]
+                race = row[2]
+                character_class = row[3]
+                level = row[4]
+                character = Character(id, name, race, character_class, level)
+                characters.append(character)
 
     def save_characters(self):
         # this will save characters to the csv file
