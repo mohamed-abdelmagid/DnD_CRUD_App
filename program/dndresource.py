940b8aa42f4d02fb7ff35eff8662ff528514ee03
Mohamed Abdelmagid <MohamedAbdelmagid12@outlook.com>
2023-04-19 14:19:12 -0400

    Created characters

--- program/dndresource.py
+++ program/dndresource.py
@@ -1,1 +1,7 @@
 # resource for DnD character list
+class Character:
+    def __init__(self, name, race, character_class, level):
+        self.name = name
+        self.race = race
+        self.character_class = character_class
+        self.level = level
